27ed7369ead57173d9d8443a4929c989cde89916
Brandon Mellus <mellusbrandon@gmail.com>
2020-11-20 15:12:53 -0500

    Design type definitions

---
+++ server/graphql/typedefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+    type Query {
+        getUsers: [User]!
+        getUser: User!
+        getFpRewards: [FpReward]
+        getFpReward: FpReward
+    }
+
+    type Mutation {
+        login: User!
+        register: User!
+        createFpReward: FpReward!
+        deleteFpReward: String!
+    }
+
+    type RegisterInput {
+        email: String!
+        username: String!
+        password: String!
+        repeatPassword: String!
+    }
+
+    type LoginInput {
+        email: String
+        username: String
+        password: String!
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        username: String!
+        password: String!
+        createdOn: String!
+        fp: Int!
+        fpRewards: [FpReward]!
+        ranking: Int!
+    }
+
+    type FpReward {
+        id: ID!
+        fp: Int!
+        reason: String
+        createdAt: String!
+    }
+`;
