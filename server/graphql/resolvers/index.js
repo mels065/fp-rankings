e8a7623b0fcc74b8d67570e60457eeb8df771d52
Brandon Mellus <mellusbrandon@gmail.com>
2020-11-24 16:44:16 -0500

    Setup resolvers for fp and ranking handling in the User typedef

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,6 +1,11 @@
 const UserResolver = require('./user');
+const User = require('../../models/user');
 
 module.exports = {
+    User: {
+        fp: async (id) => 0,
+        ranking: async (id) => 0
+    },
     Query: {
         ...UserResolver.Query
     },
