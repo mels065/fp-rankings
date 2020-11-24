88c10d8dd814d3652f98654a34c23bf494b61520
Brandon Mellus <mellusbrandon@gmail.com>
2020-11-24 14:22:08 -0500

    Allow fetching of user data

--- server/graphql/resolvers/user.js
+++ server/graphql/resolvers/user.js
@@ -1,8 +1,24 @@
 const User = require("../../models/user");
 
 const Query = {
-    getUsers: () => {},
-    getUser: (id) => {}
+    getUsers: async () => {
+        try {
+            return await User.find({});
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+    getUser: async (_, { id }) => {
+        try {
+            const user = await User.findById(id);
+            if (!user) {
+                throw new Error("User not found");
+            }
+            return user;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 };
 
 const Mutation = {
--- server/graphql/typedefs.js
+++ server/graphql/typedefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server');
 module.exports = gql`
     type Query {
         getUsers: [User]!
-        getUser: User!
+        getUser(id: ID!): User
         getFpRewards: [FpReward]
         getFpReward: FpReward
     }
